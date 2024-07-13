30f1895c5811cb993488e9f721d576c43565e5a7
Dmitry Porotnikov <dmitry.porotnikov@gmail.com>
2024-07-13 10:40:44 +0200

    Update azure_rss_generator.py

--- azure_rss_generator.py
+++ azure_rss_generator.py
@@ -21,31 +21,28 @@ def create_rss_feed(title, news_items, url):
     fg.description(f"RSS feed for {url}")
 
     for item in news_items:
-        fe = fg.add_entry()
         item_title = item.text.strip()
         item_link = item['href']
         
-        fe.title(item_title if item_title else "No title")
-        fe.link(href=f"https://azure.microsoft.com{item_link}" if item_link.startswith('/') else item_link)
-        fe.description(item_title)
-
         date_div = item.find_parent('div', class_='row')
         if date_div:
-            print(f"Date div found for item: {item.text.strip()}")
             date_p = date_div.find('div', attrs={'data-oc-token-text': ''}).find('p')
             if date_p:
                 date_str = date_p.text.strip()
                 item_date = datetime.datetime.strptime(date_str, '%b %d')
                 current_year = datetime.datetime.now().year
                 item_date = item_date.replace(year=current_year, tzinfo=datetime.timezone.utc)
+                
+                fe = fg.add_entry()
+                fe.title(item_title if item_title else "No title")
+                fe.link(href=f"https://azure.microsoft.com{item_link}" if item_link.startswith('/') else item_link)
+                fe.description(item_title)
                 fe.published(item_date)
-                print(f"Date parsed: {item_date}")
+                print(f"Item added: {item_title}, Date: {item_date}")
             else:
-                print("Date paragraph not found within div")
-                fe.published(datetime.datetime.now(datetime.timezone.utc).replace(hour=0, minute=0, second=0))
+                print(f"Date not found for item: {item_title}")
         else:
-            print(f"Date div not found for item: {item.text.strip()}")
-            fe.published(datetime.datetime.now(datetime.timezone.utc).replace(hour=0, minute=0, second=0))
+            print(f"Date div not found for item: {item_title}")
 
     return fg
 
@@ -57,7 +54,7 @@ def main():
     
     feed.rss_file('azure_updates_feed.xml')
     print("RSS feed has been created and saved as 'azure_updates_feed.xml'")
-    print(f"Number of items in feed: {len(news_items)}")
+    print(f"Number of items in feed: {len(feed.entry())}")
 
 if __name__ == "__main__":
     main()
